deb6a61933bfdb8429742a924392594e1925e27a
Sidibe Sory <sidibesoryarmel@gmail.com>
2026-02-23 16:03:08 +0000

    création des migrations

---
+++ back/database/migrations/2026_02_23_154245_espace_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('espaces', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->integer('surface');
+            $table->enum('type', [
+                'bureau',
+                'salle_reunion',
+                'conference'
+            ]);
+            $table->decimal('tarif_jour', 8, 2);
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('espaces');
+    }
+};
